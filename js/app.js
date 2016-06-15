0fc27facd0e5929abab394f96a8112da5ec92955
Scott Diallo <diallot@me.com>
2016-06-15 00:37:54 -0700

    html designed && all dependencies have been added

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    //console.log('hello world!');
+    //    $('#submit-btn').click(function (event) {
+    //        event.preventDefault();
+    //        var userSearch = $('input').val();
+    //        $('#searchResults').append('<h2>' + userSearch + '</h2>');
+    //        var empty = $('input').val('');
+    //    });
+
+
+});
